120637566c3e351af2f172a4165de8c40e50b7cb
Kaleb Hermes <hermes.kaleb@gmail.com>
2024-03-30 15:46:45 -0400

    fix: update get book script to work in 2024

--- get_kindle_books.js
+++ get_kindle_books.js
@@ -1,40 +1,46 @@
-var db = openDatabase('K4W', '3', 'thedatabase', 1024 * 1024);
+// init
+let xhr = new XMLHttpRequest();
+let domain = "https://read.amazon.com/";
+let items = [];
+let csvData = "";
 
-getAmazonCsv = function() {
-  var asinList = "";
+// function
+function getItemsList(paginationToken = null) {
+  let url =
+    domain +
+    "kindle-library/search?query=&libraryType=BOOKS" +
+    (paginationToken ? "&paginationToken=" + paginationToken : "") +
+    "&sortType=acquisition_desc&querySize=50";
+  xhr.open("GET", url, false);
+  xhr.send();
+}
 
-  db.transaction(function(tx) {
-    tx.executeSql('SELECT * FROM bookdata;', [], function(tx, results) {
-      var len = results.rows.length;
-
-      for (i = 1; i < len; i++) {
-        var asin = results.rows.item(i).asin;
-        asinList = asinList + asin + ",";
+// request result
+xhr.onreadystatechange = function () {
+  switch (xhr.readyState) {
+    case 0:
+      console.log("uninitialized");
+      break;
+    case 1:
+      console.log("loading...");
+      break;
+    case 4:
+      if (xhr.status == 200) {
+        let data = xhr.responseText;
+        data = JSON.parse(data);
+        if (data.itemsList) {
+          items.push(...data.itemsList);
+        }
+        if (data.paginationToken) {
+          getItemsList(data.paginationToken);
+        } else {
+          console.log(items.map((item) => item.asin).join(","));
+        }
+      } else {
+        console.log("Failed");
       }
-
-      var textArea = document.createElement("textarea");
-      textArea.value = asinList;
-      
-      // Avoid scrolling to bottom
-      textArea.style.top = "0";
-      textArea.style.left = "0";
-      textArea.style.position = "fixed";
-    
-      document.body.appendChild(textArea);
-      textArea.focus();
-      textArea.select();
-    
-      try {
-        var successful = document.execCommand('copy');
-        console.log(asinList);
-        console.log("List was copied to the clipboard. Return to https://kindlecovers.com and paste there!");
-      } catch (err) {
-        console.error('Oops, unable to copy, check https://github.com/kalebhermes/kindle_covers/issues for help!', err);
-      }
-    
-      document.body.removeChild(textArea);
-    });
-  });
+      break;
+  }
 };
 
-getAmazonCsv();
+getItemsList();
